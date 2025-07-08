16240e5d192b3fdf8d50c12b2a2e7ac77bd88f36
Laureano Galan <lauregalan1@gmail.com>
2025-07-08 15:50:40 -0300

    Enlace la pantlla principal con el resto de pantallas, termine la grilla de la pantalla principal y tambien agregue filtros para los botones

--- app/src/main/java/edu/unlpam/vet/ponzonosos/MostrarCatalogoActivity.java
+++ app/src/main/java/edu/unlpam/vet/ponzonosos/MostrarCatalogoActivity.java
@@ -1,6 +1,9 @@
 package edu.unlpam.vet.ponzonosos;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.GridLayout;
 import android.widget.GridView;
 
@@ -14,17 +17,18 @@ import edu.unlpam.vet.ponzonosos.adapters.AdaptadorGrid;
 import edu.unlpam.vet.ponzonosos.model.Animal;
 import edu.unlpam.vet.ponzonosos.model.AnimalDao;
 
-public class MostrarCatalogoActivity extends AppCompatActivity {
+public class MostrarCatalogoActivity extends AppCompatActivity implements View.OnClickListener{
     GridView gridView;
     AnimalDao animalDao;
     List<Animal> animals;
     AdaptadorGrid adaptadorGrid;
 
+    int tipo = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
-
 
         setContentView(R.layout.menucatalogo);
 
@@ -38,6 +42,7 @@ public class MostrarCatalogoActivity extends AppCompatActivity {
 
         gridView = findViewById(R.id.gv_catalogo);
 
+        //traemos los animalitos
         animalDao = MainActivity.getInstance().getDaoSession().getAnimalDao();
 
         animals = animalDao.queryBuilder().list();
@@ -46,5 +51,60 @@ public class MostrarCatalogoActivity extends AppCompatActivity {
 
         gridView.setAdapter(adaptadorGrid);
 
+        Button cardQueHacer = findViewById(R.id.cv_what_to_doo);
+        Button cardPrevencion = findViewById(R.id.cv_prevention_measuress);
+        Button cardArania = findViewById(R.id.cv_arania);
+        Button cardEscorpion = findViewById(R.id.cv_escorpionn);
+        Button cardSerpiente = findViewById(R.id.cv_serpientee);
+        //CardView cardTodos = findViewById(R.id.cv_todos);
+
+        cardQueHacer.setOnClickListener(this);
+        cardPrevencion.setOnClickListener(this);
+        cardArania.setOnClickListener(this);
+        cardEscorpion.setOnClickListener(this);
+        cardSerpiente.setOnClickListener(this);
+        //cardTodos.setOnClickListener(this);
     }
+
+    @Override
+
+    public void onClick(View view) {
+        int id = view.getId();
+
+        if (id == R.id.cv_what_to_doo) {
+            startActivity(new Intent(this, WhatToDoActivity.class));
+            return;
+        }
+
+        if (id == R.id.cv_prevention_measuress) {
+            startActivity(new Intent(this, PreventionMeasuresActivity.class));
+            return;
+        }
+
+        if (id == R.id.cv_arania) {
+            tipo = 1;
+        } else if (id == R.id.cv_escorpionn) {
+            tipo = 2;
+        } else if (id == R.id.cv_serpientee) {
+            tipo = 3;
+        } else {
+            tipo = 0;
+        }
+
+        actualizarGrid();
+    }
+
+    private void actualizarGrid() {
+        if (tipo == 0) {
+            animals = animalDao.queryBuilder().list(); // Todos
+        } else {
+            animals = animalDao.queryBuilder()
+                    .where(AnimalDao.Properties.Tipo.eq(tipo))
+                    .list();
+        }
+
+        adaptadorGrid.setAnimals(animals); // Necesitás este método en tu adaptador
+        adaptadorGrid.notifyDataSetChanged();
+    }
+
 }
