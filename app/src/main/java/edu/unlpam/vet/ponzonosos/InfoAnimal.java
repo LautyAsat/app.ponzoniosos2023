e880a85b89a231eaeff5a1a1b24079e536f36dc5
Simón Martin <101838886+SimonMartin1@users.noreply.github.com>
2025-07-08 09:52:02 -0300

    Fix some warnings

--- app/src/main/java/edu/unlpam/vet/ponzonosos/InfoAnimal.java
+++ app/src/main/java/edu/unlpam/vet/ponzonosos/InfoAnimal.java
@@ -2,37 +2,28 @@ package edu.unlpam.vet.ponzonosos;
 
 import android.content.Context;
 import android.content.Intent;
-
 import androidx.core.content.ContextCompat;
-import androidx.viewpager.widget.ViewPager;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.widget.Button;
+
 import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 
 
+import java.util.Objects;
+
 import edu.unlpam.vet.ponzonosos.model.Imagen;
 import edu.unlpam.vet.ponzonosos.model.Animal;
 
 public class InfoAnimal extends AppCompatActivity {
 
-    private TextView tvNombreComun, tvNombreCientifico;
-    private TextView tvTamaño, tvSintomas, tvAntidoto, tvLugarEncuentro;
-    private TextView tvAcciones, tvConfusion, tvDescripcion;
-    private ImageView ivPrincipal;
-
-    private  Drawable fondo;
-    private View tvAgresividad;
-    private Button btnVerFotos;
+//    private TextView tvDescripcion;
     private Context context;
 
     @Override
@@ -40,9 +31,9 @@ public class InfoAnimal extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.info_animal);
 
-        context = this;
+//        context = this;
 
-        Long idAnimal = (Long) getIntent().getExtras().get("obj");
+        Long idAnimal = (Long) Objects.requireNonNull(getIntent().getExtras()).get("obj");
         final Animal animal = Animal.getAnimal(idAnimal);
 
         if (animal == null) {
@@ -51,19 +42,18 @@ public class InfoAnimal extends AppCompatActivity {
         }
 
         // Vincular vistas
-        ivPrincipal = findViewById(R.id.iv_img_principal);
-        tvNombreComun = findViewById(R.id.tv_nombre_comun);
-        tvNombreCientifico = findViewById(R.id.tv_nombre_cientifico);
-        tvAgresividad = findViewById(R.id.tv_nivel_riesgo);
-        tvTamaño = findViewById(R.id.tv_tamaño);
-        tvSintomas = findViewById(R.id.tv_sintomas_picadura);
-        tvAntidoto = findViewById(R.id.tv_antidoto);
-        tvLugarEncuentro = findViewById(R.id.tv_habitat);
-        tvAcciones = findViewById(R.id.tv_acciones_picadura);
-        tvConfusion = findViewById(R.id.tv_confusion);
+        ImageView ivPrincipal = findViewById(R.id.iv_img_principal);
+        TextView tvNombreComun = findViewById(R.id.tv_nombre_comun);
+        TextView tvNombreCientifico = findViewById(R.id.tv_nombre_cientifico);
+        View tvAgresividad = findViewById(R.id.tv_nivel_riesgo);
+        TextView tvTamano = findViewById(R.id.tv_tamano);
+        TextView tvSintomas = findViewById(R.id.tv_sintomas_picadura);
+        TextView tvAntidoto = findViewById(R.id.tv_antidoto);
+        TextView tvLugarEncuentro = findViewById(R.id.tv_habitat);
+        TextView tvAcciones = findViewById(R.id.tv_acciones_picadura);
+        TextView tvConfusion = findViewById(R.id.tv_confusion);
 //        tvDescripcion = findViewById(R.id.tv_descripcion); // Solo si lo agregás
-//
-//        btnVerFotos = findViewById(R.id.btn_ver_fotos); // Solo si lo incluís en el XML
+//// Solo si lo incluís en el XML
 
         // Imagen principal
         Imagen img = animal.getPrincipalImage();
@@ -76,21 +66,18 @@ public class InfoAnimal extends AppCompatActivity {
         // Setear textos
         tvNombreComun.setText(animal.getNombre());
         tvNombreCientifico.setText(animal.getNombreC());
-        tvTamaño.setText(animal.getTamaño());
+        tvTamano.setText(animal.getTamano());
         tvSintomas.setText(animal.getEfectoPicadura());
         tvAntidoto.setText(animal.getAntidoto());
         tvLugarEncuentro.setText(animal.getLugar_encuentro());
         tvAcciones.setText(animal.getAccion_picadura());
         tvConfusion.setText(animal.getConfusion());
-        if (tvDescripcion != null)
-            tvDescripcion.setText(animal.getDescripcion());
+//        if (tvDescripcion != null)
+//            tvDescripcion.setText(animal.getDescripcion());
 
+        Drawable fondo;
         // Nivel de riesgo
         switch (animal.getAgresividad()) {
-            case 1:
-                fondo = ContextCompat.getDrawable(this, R.drawable.red_texture);
-                tvAgresividad.setBackground(fondo);
-                break;
             case 2:
                 fondo = ContextCompat.getDrawable(this, R.drawable.yellow_texture);
                 tvAgresividad.setBackground(fondo);
@@ -106,8 +93,8 @@ public class InfoAnimal extends AppCompatActivity {
         }
 
         // Botón de ver fotos
-        if (btnVerFotos != null) {
-            btnVerFotos.setOnClickListener(view -> {
+        if (ivPrincipal != null) {
+            ivPrincipal.setOnClickListener(view -> {
                 if (!animal.getImages().isEmpty()) {
                     Intent intent = new Intent(getApplicationContext(), Galeria.class);
                     intent.putExtra("img", animal.getId());
